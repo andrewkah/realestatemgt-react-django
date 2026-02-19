56f1ff33715bb90128781940efddcd62c4ffc4b2
andrewkah <andrewkah2022@gmail.com>
2026-02-19 14:29:24 +0300

    Feat: Enhance lead assignment strategies and update LeadCaptureView to LeadViewSet

--- backend/apps/users/urls.py
+++ backend/apps/users/urls.py
@@ -1,18 +1,34 @@
 from rest_framework_simplejwt.views import TokenRefreshView
 from django.urls import path
+# from rest_framework.routers import DefaultRouter
 from apps.users import views
 
+"""
+DefaultRouter simplifies the URL patterns for API views. Auto generates urls for views based on their names and HTTP methods"""
+# router = DefaultRouter()
+# router.register("lead", views.LeadViewSet, basename="lead")
+
 urlpatterns = [
-    path('token/', views.TokenObtainPairView.as_view(), name='token_obtain_pair'),
-    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('register/', views.RegisterView.as_view(), name='register'),
+    path("token/", views.TokenObtainPairView.as_view(), name="token_obtain_pair"),
+    path("token/refresh/", TokenRefreshView.as_view(), name="token_refresh"),
+    path("register/", views.RegisterView.as_view(), name="register"),
     path("verify-email/", views.VerifyUserEmail.as_view(), name="verify-email"),
-    path('password-reset/', views.PasswordResetRequest.as_view(), name="password-reset"),
-    path('password-reset-confirm/<uuid64>/<token>/', views.PasswordResetConfirm.as_view(), name='password-reset-confirm'),
-    path('set-password/', views.SetNewPassword.as_view(), name="set-password"),
-    path('login/', views.LoginUserView.as_view(), name='login'),
-    path('logout/', views.LogoutUserView.as_view(), name='logout'),
-    path('profile/update/', views.UpdateUserProfile.as_view(), name='update-profile'),
-    path('lead-capture/', views.LeadCaptureView.as_view(), name='lead-capture'),
-    path('dashboard/', views.dashboard, name='dashboard'),
+    path(
+        "password-reset/", views.PasswordResetRequest.as_view(), name="password-reset"
+    ),
+    path(
+        "password-reset-confirm/<uuid64>/<token>/",
+        views.PasswordResetConfirm.as_view(),
+        name="password-reset-confirm",
+    ),
+    path("set-password/", views.SetNewPassword.as_view(), name="set-password"),
+    path("login/", views.LoginUserView.as_view(), name="login"),
+    path("logout/", views.LogoutUserView.as_view(), name="logout"),
+    path("profile/update/", views.UpdateUserProfile.as_view(), name="update-profile"),
+    path(
+        "lead-capture/",
+        views.LeadViewSet.as_view({"post": "create"}),
+        name="lead-capture",
+    ),
+    path("dashboard/", views.dashboard, name="dashboard"),
 ]
--- backend/apps/users/views.py
+++ backend/apps/users/views.py
@@ -5,11 +5,11 @@ from .models import OneTimePassword, User, Profile
 from .serializers import LeadCaptureSerializer, LoginSerializer, LogoutSerializer, PasswordResetSerializer, ProfileSerializer, RegisterSerializer, MyTokenObtainPairSerializer, SetNewPasswordSerializer, UpdateUserProfileSerializer
 
 from rest_framework_simplejwt.views import TokenObtainPairView
-from rest_framework.decorators import api_view, permission_classes
+from rest_framework.decorators import api_view, permission_classes, action
 from rest_framework.permissions import AllowAny, IsAuthenticated
-from rest_framework import generics, status
+from rest_framework import generics, status, viewsets
 from rest_framework.response import Response
-from .utils import send_otp_to_user
+from .utils import send_otp_to_user, round_robin_agent_assignment, load_balanced_assignment, skill_based_assignment
 from django.utils.http import urlsafe_base64_decode
 from django.contrib.auth.tokens import PasswordResetTokenGenerator
 from django.utils.encoding import force_str, DjangoUnicodeDecodeError
@@ -109,19 +109,46 @@ class UpdateUserProfile(generics.UpdateAPIView):
     def get_object(self):
         return self.request.user.profile
 
-class LeadCaptureView(generics.CreateAPIView):
+class LeadViewSet(viewsets.ModelViewSet):
     permission_classes = [AllowAny]
     serializer_class = LeadCaptureSerializer
 
-    def post(self, request, *args, **kwargs):
+    def create(self, request, *args, **kwargs):
         try:
             serializer = self.serializer_class(data=request.data)
             if serializer.is_valid(raise_exception=True):
-                serializer.save()
-                return Response(serializer.data, status=status.HTTP_200_OK)
+                lead = serializer.save()
+                # Choose assignment strategy
+                assignment_strategy = request.date.get("assignment_strategy", "round_robin")
+                if assignment_strategy == 'round_robin':
+                    agent = round_robin_agent_assignment(lead)
+                elif assignment_strategy == 'load_balanced':
+                    agent = load_balanced_assignment(lead)
+                elif assignment_strategy == 'skill_based':
+                    agent = skill_based_assignment(lead)
+                # Send email
+                # Refresh serializer data
+                serializer = self.get_serializer(lead)
+                return Response({'Lead': serializer.data, "Agent": agent, 'message': f'Lead assigned to {agent.profile.first_name}' if agent else "No agent available"}, status=status.HTTP_201_CREATED)
             return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
         except Exception as e:
             return Response({"message": str(e)}, status=status.HTTP_400_BAD_REQUEST)
+    
+    @action(detail=True, methods=['get'])
+    def reassign(self, request, pk=None):
+        """ Manually reassign lead to agent """
+        lead = self.get_object()
+        serializer = self.get_serializer(lead, many=True)
+        agent_id = request.data.get('agent_id')
+        try:
+            agent = Agent.objects.get(id=agent_id)
+            lead.assigned_agent = agent
+            lead.save()
+            agent.total_leads += 1
+            agent.save()
+            return Response({"message": f"Lead re-assigned to {agent.profile.first_name}", "Lead": self.get_serializer(lead).data}, status=status.HTTP_200_OK)
+        except Agent.DoesNotExist:
+            return Response({"message": "Agent not found"}, status=status.HTTP_404_NOT_FOUND)
 
 # Create your views here.
 @api_view(['GET', 'POST'])
